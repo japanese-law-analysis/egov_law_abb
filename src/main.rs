24e7ad879a1e18cb8b9f16b6c8645bfc4264dfc0
puripuri2100 <puripuri2100@gmail.com>
2023-05-23 00:20:34 +0900

    fmt

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,22 @@
 //! ```sh
 //! cargo install --git "https://github.com/japanese-law-analysis/egov_law_abb.git"
 //! ```
-//! 
+//!
 //! # Use
 //!
 //! ```sh
 //!   egov_law_abb --output "egov_abb.json"
 //! ```
-//! 
+//!
 //! のようにして使用します。
-//! 
+//!
 //! # 生成される情報
-//! 
+//!
 //! 以下の2つのフィールドを持つobjectのリストが生成されます
-//! 
+//!
 //! - `num`：法令番号
 //! - `abbs`：略称の文字列のリスト
-//! 
+//!
 //! ---
 //! [MIT License](https://github.com/japanese-law-analysis/listup_precedent/blob/master/LICENSE)
 //! (c) 2023 Naoki Kaneko (a.k.a. "puripuri2100")
